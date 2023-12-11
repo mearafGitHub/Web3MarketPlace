0566a65a9c57def58ad8c63db0993d1026715726
mearaf <mearaftadewos@gmail.com>
2023-12-11 03:06:55 +0300

    Clean up delopy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ async function main() {
 
   const IBLOXXToken = await ethers.getContractFactory("IBLOXXToken"); 
   const Auction = await ethers.getContractFactory("Auction"); 
-  
+
 
   // Deploy IBLOXXTokenContrac contract  -> UUPS proxy 
   console.log("\nDeploying IBLOXXToken..."); 
@@ -18,38 +18,25 @@ async function main() {
  
   console.log("\nIBLOXXToken deployed:", proxyIBLOXXTokenContract);
 
-  const mintTx = await proxyIBLOXXTokenContract.safeMint(process.env.MINTER_ROLE, "https://app.infura.io/"); 
-  const resultNFT = await mintTx.wait(); 
-  console.log("\nSafe mint NFT:\n", resultNFT)
-  console.log(`\nNFT minted to address "${resultNFT.to}".`); 
-  
-  const nft = await proxyIBLOXXTokenContract.mint(10); 
-  const NFT = await nft.wait(); 
-  console.log("\nMint NFT:\n", NFT)
-  console.log(`\n10 NFTs minted to address "${NFT.to}".`); 
-
-  // Verify nIBLOXXToken Contract:
-  const contract_address = proxyIBLOXXTokenContract.target; 
-
+  // Verify IBLOXXToken Contract:
+  const IBLOXXToken_contract_address = proxyIBLOXXTokenContract.target; 
+ 
 
   // Deploy Auction contract  -> UUPS proxy
-  // console.log("\nDeploying Auction Contract..."); 
-  // // Deploy the implementation contract via UUPS proxy 
-  // const proxyAuctionContract = await upgrades.deployProxy(Auction, 
-  //   [process.env.DEFAULT_ADMIN, process.env.UPGRADER_ROLE], 
-  //   { initializer: 'initialize' }
-  //   ); 
+  console.log("\nDeploying Auction Contract..."); 
+  // Deploy the implementation contract via UUPS proxy 
+  const proxyAuctionContract = await upgrades.deployProxy(Auction, 
+    [process.env.DEFAULT_ADMIN, process.env.UPGRADER_ROLE], 
+    { initializer: 'initialize' }
+    ); 
 
-  // console.log("\nProxyAuctionContract contract deployed. Details:\n", proxyAuctionContract);
+  console.log("\nProxyAuctionContract contract deployed. Details:\n", proxyAuctionContract);
 
-  // const resultAuction = await proxyAuctionContract.deployed(); 
-  // console.log("Result of de NFT:\n", resultAuction) 
-
-  // const startAuction = await proxyAuction.start_auction( _nft_id, IERC721 _nft, uint _end_date, uint _initial_price);  
-  // console.log(`\nNFT minted to address ${defaultAdmin}. NFT Contract address: ${proxyIBLOXXToken.address()}`); 
+  // Verify Auction Contract:
+  const Auction_contract_address = proxyAuctionContract.target; 
 
 } 
   
 main() .then(() => process.exit(0)) .catch((error) => { console.error(error); process.exit(1); }); 
 
-// npx hardhat run scripts/deploy.js --network goerli  
+// npx hardhat run scripts/deploy.js --network goerli_quick_node  
