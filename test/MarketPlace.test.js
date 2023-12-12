119885854c4bc0c486bda18f5f7130ab1a22be2b
mearaf <mearaftadewos@gmail.com>
2023-12-12 21:22:30 +0300

    Test cases - initial layout

--- test/MarketPlace.test.js
+++ test/MarketPlace.test.js
@@ -2,9 +2,29 @@ const {time, loadFixture,} = require("@nomicfoundation/hardhat-toolbox/network-h
   const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
   const { expect } = require("chai");
   
-  describe("IBloxToken", function () {
+  describe("MarketPlace", function () {
+
+    async function createNFT() {
+  
+    }
 
     async function startAuction() {
+  
+    }
+
+    async function getAllAuctionNFTs() {
+  
+    }
+
+    async function getAllFixedPriceNFT() {
+  
+    }
+
+    async function getAuctionEndTime() {
+  
+    }
+
+    async function getBiddersOf() {
   
     }
   
