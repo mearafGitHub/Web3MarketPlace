e9cd2d8ba86167f63f8c7f74db33d11c552c5f81
mearaf <mearaftadewos@gmail.com>
2023-12-12 04:50:55 +0300

    Added Web3 setups

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const ethers = require('ethers');
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
+
 app.use(express.json());
+require('dotenv').config();
+
+const QUICK_NODE_URL = process.env.QUICK_NODE_URL;
+const TEST_ACC_PRIVATE_KEY = process.env.TEST_ACC_PRIVATE_KEY;
+
+const contractAddressIIBLOXXToken = process.env.MARKET_PLACE_CONTRACT_ADDRESS;
+const contractAddressMarektPlace = process.env.IBLOXX_CONTRACT_ADDRESS;
+
+const provider = new ethers.providers.JsonRpcProvider(QUICK_NODE_URL);
+const signer = new ethers.Wallet(TEST_ACC_PRIVATE_KEY, provider);
+
+// MarektPlace contract
+const {abi} = require("./artifacts/contracts/MarektPlace.sol/MarektPlace.json");
+const contractInstanceMarektPlace = new ethers.Contract(contractAddress, abi, signer);
+
+// IBLOXXToken contract
+const {abi} = require("./artifacts/contracts/IBLOXXToken.sol/IBLOXXToken.json");
+const contractInstanceIBLOXXToken = new ethers.Contract(contractAddress, abi, signer);
 
 const port = process.env.PORT;
 app.listen(
