b89b46c4b2116237beb191f21e35f94035b67be9
mearaf <mearaftadewos@gmail.com>
2023-12-12 20:35:12 +0300

    Node js server interacting with Market Place contract

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ app.listen(
 )
 
 app.get("/auctionNfts", async function (request, result) {
-    const sample = {}
     try {
         const nfts = await contractInstanceMarketPlace.getAllAuctionNFTs();
         result.status(200).send(nfts);
@@ -39,43 +38,82 @@ app.get("/auctionNfts", async function (request, result) {
     }
 });
 
-app.get('/fixedPriceNfts', async function(request, result, next){
-    const sample = {}
-    result.send(sample);
+app.get('/fixedPriceNfts', async function(request, result) {
+    try {
+        const nfts = await contractInstanceMarketPlace.getAllFixedPriceNFT();
+        result.status(200).send(nfts);
+    } catch (error) {
+        console.error("\nFound Error:\n",error);
+    }
 });
 
-app.post("/creatNFT", function (request, result) {
-    
+app.post("/createNFT", async function (request, result) { 
+    try {
+        // Take parameters in request body
+        const {price, nftName, isForAuction, auctionEndTime} = request.body;
+        const creatNFTTransaction = await contractInstanceMarketPlace.createNFT(price, nftName, isForAuction, auctionEndTime);
+        // await creatNFTTransaction.wait();
+        
+        result.status(201).send(creatNFTTransaction);
+    } catch (error) {
+        console.error("\n createNFT Found Error:\n",error);
+    }
+});
+
+app.post('/createAuction', async function(request, result){
     const sample = {}
     result.status(201).send(sample);
 });
 
-app.post('/createAuction', async function(request, result, next){
-    const sample = {}
-    result.status(201).send(sample);
+app.get('/getAuctionEndTime/:nftID', async function(request, result){
+
+    try {
+        const id = request.params.nftID;
+        const nfts = await contractInstanceMarketPlace.getAuctionEndTime(nftId);
+        result.status(200).send(nfts);
+    } catch (error) {
+        console.error("\n getAuctionEndTime Found Error:\n",error);
+    }
 });
 
-app.get('/getAuctionEndTime', async function(request, result, next){
-    const sample = {}
-    result.status(200).send(sample);
+app.get('/getBiddersOf/:nftID', async function(request, result){
+    try {
+        const _nftID = request.params.nftID;
+        console.log("Got id: ", _nftID);
+        const bidders = await contractInstanceMarketPlace.getBiddersOf(_nftID);
+        result.status(200).send(bidders);
+    } catch (error) {
+        console.error("\ngetBiddersOf Found Error:\n",error);
+    }
 });
 
-app.get('/bidders', async function(request, result,  next){
-    const sample = {}
-    result.status(200).send(sample);
+app.post('/startAuction', async function(request, result){
+    try {
+        const {nftId, auctionEndsAt, initialPrice} = request.body;
+        const nfts = await contractInstanceMarketPlace.startAuction ( nftId, auctionEndsAt, initialPrice);
+        result.status(200).send(nfts);
+    } catch (error) {
+        console.error("\nendAuctionFound Error:\n",error);
+    }
 });
 
-app.post('/startAuction', async function(request, result, next){
-    const sample = {}
-    result.status(201).send(sample);
+app.post('/endAuction', async function(request, result){
+    try {
+        const {nftId} = request.body;
+        const nfts = await contractInstanceMarketPlace.endAuction(nftId);
+        result.status(200).send(nfts);
+    } catch (error) {
+        console.error("\nendAuctionFound Error:\n",error);
+    }
 });
 
-app.post('/endAuction', async function(request, result, next){
-    const sample = {}
-    result.status(201).send(sample);
-});
 
-app.get('/withdrawCollateral', async function(request, result, next){
-    const sample = {}
-    result.status(201).send(sample);
+app.get('/withdrawCollateral', async function(request, result){
+    try {
+        const {nftId, bidderAddress} = request.body;
+        const nfts = await contractInstanceMarketPlace.withdraw(nftId, bidderAddress);
+        result.status(200).send(nfts);
+    } catch (error) {
+        console.error("\nwithdrawCollateral Found Error:\n",error);
+    }
 });
